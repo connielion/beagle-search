d55a7606711d97fe3b2dff7bf83512e27d1ae4de
Connie <clai011@ucr.edu>
2019-03-24 04:50:58 -0700

    input box, logo, button created

--- index.php
+++ index.php
@@ -5,14 +5,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Poodle</title>
+    <title>Beagle</title>
+
 </head>
 
 <body>
 
     <div class="main">
-        <div class="logocontainer">
-            <img src="" alt="">
+        <div class="logoContainer">
+            <img src="img/logo.svg" alt="logo" class="logo">
+        </div>
+
+        <div class="searchContainer">
+            <!--send data to search.php via GET-->
+            <form action="search.php" method="GET">
+                <input type="text" class="searchBox" name="term" placeholder="Search something...">
+
+                <input type="submit" class="searchBtn" value="Search">
+            </form>
+
         </div>
     </div>
 </body>
