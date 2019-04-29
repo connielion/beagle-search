8762ec025e069a8026dff20b6e51cb14b075f0ac
Connie <clai011@ucr.edu>
2019-04-29 04:07:19 -0700

    search page structure

--- search.php
+++ search.php
@@ -1,4 +1,40 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-// echo - print in php, get 'term' from url
-echo $_GET["term"];
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Beagle</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+
+    <div class="wrapper">
+
+        <div class="header">
+            <div class="headerContent">
+                <div class="logoContainer">
+                    <a href="index.php">
+                        <img src="img/logo3.svg" alt="logo" class="logo">
+                    </a>
+                </div>
+                <div class="search-cont">
+                    <form action="search.php" method="GET">
+                        <div class="searchbar-cont">
+                            <input type="text" class="searchbox" name="term">
+                            <button>
+                                Sniff
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
