93f29ef33b1e99f2d24bc904e7a5874ebcc0e24a
Connie <clai011@ucr.edu>
2019-04-29 03:54:50 -0700

    home page styling

--- index.php
+++ index.php
@@ -15,16 +15,16 @@
         <div class="main">
 
             <div class="logoContainer">
-                <img src="img/logo.svg" alt="logo" class="logo">
+                <img src="img/logo3.svg" alt="logo" class="logo">
             </div>
 
             <div class="searchContainer">
                 <!--send data to search.php via GET-->
                 <form action="search.php" method="GET">
                     <!--searchBox-->
-                    <input type="text" class="searchBox" name="term" placeholder="Search something...">
+                    <input type="text" class="searchBox" name="term" placeholder="What should we sniff out?">
                     <!--searchBtn-->
-                    <input type="submit" class="searchBtn" value="Go Sniff">
+                    <input type="submit" class="searchBtn" value="Sniff it out!">
                 </form>
 
             </div>
